588dfa9ad05ddd0a92449a28bb83f908d74a1c23
ezequielpato <ezequiel.salomon@gmail.com>
2020-03-28 15:18:40 -0300

    probando

--- 2803-http-node-2.js
+++ 2803-http-node-2.js
@@ -2,11 +2,13 @@ const http = require("http");
 const fs = require("fs");
 
 let server = http.createServer((req, res) => {
-    html = fs.readFileSync(__dirname + "/index.html", "utf-8");
-    res.writeHead(200, { "Content-Type": "text/html" });
-    if (req.url === "/contacto") {
-        res.end(html);
-    } else if (req.url === "/") res.end("Bienvenidosssssss!!!!!");
+  html = fs.readFileSync(__dirname + "/index.html", "utf-8");
+  res.writeHead(200, { "Content-Type": "text/html" });
+  if (req.url === "/contacto") {
+    res.end(html);
+  } else if (req.url === "/") res.end("Bienvenidosssssss!!!!!");
 });
 
 server.listen(3030, "localhost");
+
+//mauro puto  15:17hs 280320
